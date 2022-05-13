82af4d5db9eb45662c1ca0ff4b11157b9b965471
gitlearnersurbhi <prasadsurbhi2001@gmail.com>
2022-05-13 14:10:20 +0530

    second commit

--- SigCloud/s.py
+++ SigCloud/s.py
@@ -3,3 +3,9 @@ def sum(val1, val2):
 
 def diff(val1 , val2):
     return int(val1) - int(val2)
+
+def mul(val1 , val2):
+    return int(val1) * int(val2)
+
+def div(val1 , val2):
+    return int(val1) / int(val2)
